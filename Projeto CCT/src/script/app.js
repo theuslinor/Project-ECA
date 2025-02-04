96caa8bad53bd5ab758c967dc615cf966c64c31d
theuslino <matheusdossantos3157@gmail.com>
2025-02-04 19:10:09 -0300

    nova nav-bar

--- Projeto CCT/src/script/app.js
+++ Projeto CCT/src/script/app.js
@@ -1,4 +1,5 @@
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav");
 
-hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('#header')
+    header.classList.toggle('rolagem', window.scrollY > 500)
+})
